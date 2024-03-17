495587c62eefb88e72bdac707d187a42e4960b6b
ahmad <aalmadani111@gmail.com>
2024-03-17 01:06:33 +0100

    test model bert with sentiment , write any text and get the sentimnet of the text positive or negative

---
+++ bert_sentiment.py
@@ -0,0 +1,46 @@
+from transformers import BertTokenizer , TFBertForSequenceClassification
+
+model_name = 'nlptown/bert-base-multilingual-uncased-sentiment'
+
+
+# Spliter --->>
+tokenizer = BertTokenizer.from_pretrained(model_name)
+
+# model use name 
+# nedded libraries :
+# pip install tf-keras
+#pip install TensorFlow
+model = TFBertForSequenceClassification.from_pretrained(model_name)
+
+# test the model 
+
+text = input('Wirte Your text hier :')
+text_input = tokenizer(text)
+
+
+input_ids = text_input['input_ids']
+
+#prediction sentiment of input_ids
+predictions = model.predict([input_ids])
+logits = predictions.logits
+
+import numpy as np
+
+predicted_class = np.argmax(logits)
+
+print(predicted_class) # output number 3 if the text example as  (i like my work ) 
+
+
+# how bert work ?
+
+sentiment_mapping = {
+    0:'evry negative',
+    1:'negative',
+    2:'neutral',
+    3:'positive',
+    4:'very positive'
+}
+
+predicted_sentiment = sentiment_mapping[predicted_class]
+
+print(predicted_sentiment)
